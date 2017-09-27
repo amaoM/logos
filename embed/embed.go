6bcba184bde3b2c0d2be34355bce76e6d7bb0d6d
amaoM <chapa0106@gmail.com>
2017-09-27 18:47:33 +0900

    Add an embed using interface

--- embed/embed.go
+++ embed/embed.go
@@ -1,5 +1,7 @@
 package embed
 
+import "fmt"
+
 // Human ...
 type Human struct {
 	Type string
@@ -16,11 +18,62 @@ type Woman struct {
 }
 
 // Talk ...
-func (human *Human) Talk() string {
-	return "I am a " + human.Type
+func (human *Human) Talk() bool {
+	fmt.Println("I am a " + human.Type)
+	return true
 }
 
-func logos1() string {
+func logos1() bool {
 	man := Man{&Human{Type: "man"}}
 	return man.Talk()
+}
+
+type Animal interface {
+	Eat()
+}
+
+type Dog struct {
+}
+
+type Cat struct {
+}
+
+func (d *Dog) Eat() bool {
+	fmt.Println("I eating dog foods.")
+	return true
+}
+
+func (c *Cat) Eat() bool {
+	fmt.Println("I eating cat foods.")
+	return true
+}
+
+func (h *Human) Eat() bool {
+	fmt.Println("I eating tenpura")
+	return true
+}
+
+func (m *Man) Ean() bool {
+	fmt.Println("I eating steaks")
+	return true
+}
+
+func (wm *Woman) Eat() bool {
+	fmt.Println("I eating cakes")
+	return true
+}
+
+func logos2() bool {
+	dog := new(Dog)
+	cat := new(Cat)
+	human := new(Human)
+	man := new(Man)
+	woman := new(Woman)
+	dog.Eat()
+	cat.Eat()
+	human.Eat()
+	man.Eat()
+	woman.Eat()
+
+	return true
 }
--- embed/embed_test.go
+++ embed/embed_test.go
@@ -9,3 +9,8 @@ func TestLogos1(t *testing.T) {
 	actual := logos1()
 	fmt.Println(actual)
 }
+
+func TestLogos2(t *testing.T) {
+	actual := logos2()
+	fmt.Println(actual)
+}
