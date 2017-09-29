77964bde0da8d8a514e1353aef038f08c7cdb625
amaoM <chapa0106@gmail.com>
2017-09-29 23:27:40 +0900

    Add the row to input queries

--- termbox/termbox.go
+++ termbox/termbox.go
@@ -7,26 +7,31 @@ import (
 	termbox "github.com/nsf/termbox-go"
 )
 
+// Category ...
 type Category struct {
 	ID     int
 	Name   string
 	Cursor bool
 }
 
+// Language ...
 type Language struct {
 	ID     int
 	Name   string
 	Cursor bool
 }
 
+// Infrastructure ...
 type Infrastructure struct {
 	ID     int
 	Name   string
 	Cursor bool
 }
 
+// Item ...
 type Item struct{}
 
+// Screen ...
 type Screen struct {
 	Categories       []Category
 	Languages        []Language
@@ -34,6 +39,32 @@ type Screen struct {
 	RowCount         int
 	CurrentRowNumber int
 	ItemName         string
+}
+
+func createQueryRow() {
+	for i, r := range "QUERY>" {
+		termbox.SetCell(i, 0, r, termbox.ColorWhite, termbox.ColorBlack)
+	}
+}
+
+func (s *Screen) changeCursor() {
+	switch s.ItemName {
+	case "Categories":
+		for i := range s.Categories {
+			s.Categories[i].Cursor = false
+		}
+		s.Categories[s.CurrentRowNumber-1].Cursor = true
+	case "Languages":
+		for i := range s.Languages {
+			s.Languages[i].Cursor = false
+		}
+		s.Languages[s.CurrentRowNumber-1].Cursor = true
+	case "Infrastructures":
+		for i := range s.Infrastructures {
+			s.Infrastructures[i].Cursor = false
+		}
+		s.Infrastructures[s.CurrentRowNumber-1].Cursor = true
+	}
 }
 
 func main() {
@@ -67,15 +98,16 @@ func main() {
 
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
 
+	createQueryRow()
 	for i, category := range screen.Categories {
 		for ii, r := range category.Name {
-			termbox.SetCell(ii, i, r, termbox.ColorWhite, termbox.ColorBlack)
+			termbox.SetCell(ii, i+1, r, termbox.ColorWhite, termbox.ColorBlack)
 		}
 	}
 
 	screen.RowCount = len(screen.Categories)
 	screen.ItemName = "Categories"
-	screen.CurrentRowNumber = -1
+	screen.CurrentRowNumber = 0
 
 	width, _ := termbox.Size()
 	cell := termbox.CellBuffer()
@@ -93,39 +125,24 @@ loop:
 
 			// Down
 			if ev.Key == 65516 {
-				if screen.CurrentRowNumber >= screen.RowCount-1 {
+				if screen.CurrentRowNumber >= screen.RowCount {
 					break
 				}
 				screen.CurrentRowNumber++
 				for i := 0; i < width; i++ {
-					if screen.CurrentRowNumber > 0 {
+					if screen.CurrentRowNumber > 1 {
 						termbox.SetCell(i, screen.CurrentRowNumber-1, cell[(screen.CurrentRowNumber-1)*width+i].Ch, termbox.ColorWhite, termbox.ColorBlack)
 					}
 					termbox.SetCell(i, screen.CurrentRowNumber, cell[screen.CurrentRowNumber*width+i].Ch, termbox.ColorWhite, termbox.ColorWhite)
 				}
 
-				switch screen.ItemName {
-				case "Categories":
-					for i := range screen.Categories {
-						screen.Categories[i].Cursor = false
-					}
-					screen.Categories[screen.CurrentRowNumber].Cursor = true
-				case "Languages":
-					for i := range screen.Languages {
-						screen.Languages[i].Cursor = false
-					}
-					screen.Languages[screen.CurrentRowNumber].Cursor = true
-				case "Infrastructures":
-					for i := range screen.Infrastructures {
-						screen.Infrastructures[i].Cursor = false
-					}
-					screen.Infrastructures[screen.CurrentRowNumber].Cursor = true
-				}
+				screen.changeCursor()
+
 			}
 
 			// Up
 			if ev.Key == 65517 {
-				if 0 >= screen.CurrentRowNumber {
+				if 1 >= screen.CurrentRowNumber {
 					break
 				}
 				screen.CurrentRowNumber--
@@ -134,34 +151,18 @@ loop:
 					termbox.SetCell(i, screen.CurrentRowNumber+1, cell[(screen.CurrentRowNumber+1)*width+i].Ch, termbox.ColorWhite, termbox.ColorBlack)
 				}
 
-				switch screen.ItemName {
-				case "Categories":
-					for i := range screen.Categories {
-						screen.Categories[i].Cursor = false
-					}
-					screen.Categories[screen.CurrentRowNumber].Cursor = true
-				case "Languages":
-					for i := range screen.Languages {
-						screen.Languages[i].Cursor = false
-					}
-					screen.Languages[screen.CurrentRowNumber].Cursor = true
-				case "Infrastructures":
-					for i := range screen.Infrastructures {
-						screen.Infrastructures[i].Cursor = false
-					}
-					screen.Infrastructures[screen.CurrentRowNumber].Cursor = true
-				}
+				screen.changeCursor()
 			}
 
 			// Right
 			if ev.Key == 65514 {
-				if 0 > screen.CurrentRowNumber {
+				if 1 > screen.CurrentRowNumber {
 					break
 				}
 
 				switch screen.ItemName {
 				case "Categories":
-					screen.Categories[screen.CurrentRowNumber].Cursor = true
+					screen.Categories[screen.CurrentRowNumber-1].Cursor = true
 
 					runes := []rune{}
 					for i := 0; i < width; i++ {
@@ -171,41 +172,43 @@ loop:
 					switch strings.TrimSpace(string(runes)) {
 					case "Languages":
 						termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+						createQueryRow()
 						cursor := false
 						for i, language := range screen.Languages {
 							for ii, r := range language.Name {
-								termbox.SetCell(ii, i, r, termbox.ColorWhite, termbox.ColorBlack)
+								termbox.SetCell(ii, i+1, r, termbox.ColorWhite, termbox.ColorBlack)
 							}
 							if language.Cursor {
 								cursor = true
-								screen.CurrentRowNumber = i
+								screen.CurrentRowNumber = i + 1
 								for iii := 0; iii < width; iii++ {
-									termbox.SetCell(iii, i, cell[i*width+iii].Ch, termbox.ColorWhite, termbox.ColorWhite)
+									termbox.SetCell(iii, i+1, cell[(i+1)*width+iii].Ch, termbox.ColorWhite, termbox.ColorWhite)
 								}
 							}
 						}
 						if !cursor {
-							screen.CurrentRowNumber = -1
+							screen.CurrentRowNumber = 0
 						}
 						screen.ItemName = "Languages"
 						screen.RowCount = len(screen.Languages)
 					case "Infrastructures":
 						termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+						createQueryRow()
 						cursor := false
 						for i, infrastructure := range screen.Infrastructures {
 							for ii, r := range infrastructure.Name {
-								termbox.SetCell(ii, i, r, termbox.ColorWhite, termbox.ColorBlack)
+								termbox.SetCell(ii, i+1, r, termbox.ColorWhite, termbox.ColorBlack)
 							}
 							if infrastructure.Cursor {
 								cursor = true
-								screen.CurrentRowNumber = i
+								screen.CurrentRowNumber = i + 1
 								for iii := 0; iii < width; iii++ {
-									termbox.SetCell(iii, i, cell[i*width+iii].Ch, termbox.ColorWhite, termbox.ColorWhite)
+									termbox.SetCell(iii, i+1, cell[(i+1)*width+iii].Ch, termbox.ColorWhite, termbox.ColorWhite)
 								}
 							}
 						}
 						if !cursor {
-							screen.CurrentRowNumber = -1
+							screen.CurrentRowNumber = 0
 						}
 						screen.ItemName = "Infrastructures"
 						screen.RowCount = len(screen.Infrastructures)
@@ -217,14 +220,15 @@ loop:
 			if ev.Key == 65515 {
 				if screen.ItemName == "Languages" || screen.ItemName == "Infrastructures" {
 					termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+					createQueryRow()
 					for i, category := range screen.Categories {
 						for ii, r := range category.Name {
-							termbox.SetCell(ii, i, r, termbox.ColorWhite, termbox.ColorBlack)
+							termbox.SetCell(ii, i+1, r, termbox.ColorWhite, termbox.ColorBlack)
 						}
 						if category.Cursor {
-							screen.CurrentRowNumber = i
+							screen.CurrentRowNumber = i + 1
 							for iii := 0; iii < width; iii++ {
-								termbox.SetCell(iii, i, cell[i*width+iii].Ch, termbox.ColorWhite, termbox.ColorWhite)
+								termbox.SetCell(iii, i+1, cell[(i+1)*width+iii].Ch, termbox.ColorWhite, termbox.ColorWhite)
 							}
 						}
 					}
